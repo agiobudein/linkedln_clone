3393fb0560e719b9f220ee2628d70f7594cadbd4
Dein Agiobu <agiobudein@gmail.com>
2024-12-03 08:25:04 +0100

    add spring boot initializer and vite setup

---
+++ backend/src/main/java/com/tamjayz/linkedln/LinkedlnApplication.java
@@ -0,0 +1,13 @@
+package com.tamjayz.linkedln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedlnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedlnApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tamjayz/linkedln/LinkedlnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamjayz.linkedln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedlnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
